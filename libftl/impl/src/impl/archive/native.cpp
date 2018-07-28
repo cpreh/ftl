ee6e0a7fdbfcff1d3c6adc72acaf14f0baa1bde3
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-07-28 16:08:14 +0200

    Use an istringstream for now

--- libftl/impl/src/impl/archive/native.cpp
+++ libftl/impl/src/impl/archive/native.cpp
@@ -1,12 +1,16 @@
 #include <libftl/error.hpp>
 #include <libftl/archive/base.hpp>
 #include <libftl/archive/entry.hpp>
+#include <libftl/archive/extract.hpp>
+#include <libftl/archive/file.hpp>
 #include <libftl/archive/index.hpp>
 #include <libftl/archive/path.hpp>
 #include <libftl/impl/archive/native.hpp>
 #include <fcppt/from_std_string.hpp>
+#include <fcppt/make_ref.hpp>
 #include <fcppt/make_unique_ptr.hpp>
 #include <fcppt/reference_impl.hpp>
+#include <fcppt/reference_to_base.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/unique_ptr_impl.hpp>
 #include <fcppt/unique_ptr_to_base.hpp>
@@ -17,11 +21,13 @@
 #include <fcppt/either/object_impl.hpp>
 #include <fcppt/filesystem/open.hpp>
 #include <fcppt/filesystem/path_to_string.hpp>
+#include <fcppt/io/buffer.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <boost/filesystem/path.hpp>
 #include <fstream>
 #include <ios>
 #include <istream>
+#include <sstream>
 #include <utility>
 #include <fcppt/config/external_end.hpp>
 
@@ -89,7 +95,7 @@ libftl::impl::archive::native::open(
 			)
 			{
 				return
-					fcppt::either::map(
+					fcppt::either::bind(
 						fcppt::either::from_optional(
 							fcppt::container::find_opt_mapped(
 								this->index_,
@@ -116,6 +122,8 @@ libftl::impl::archive::native::open(
 							}
 						),
 						[
+							&_path,
+							this,
 							&_stream
 						](
 							fcppt::reference<
@@ -123,21 +131,60 @@ libftl::impl::archive::native::open(
 							> const _entry
 						)
 						{
-							_stream.seekg(
-								_entry.get().offset_.get()
-							);
-
 							return
-								fcppt::unique_ptr_to_base<
-									std::istream
-								>(
-									fcppt::make_unique_ptr<
-										std::ifstream
-									>(
-										std::move(
-											_stream
-										)
+								fcppt::either::map(
+									fcppt::either::from_optional(
+										libftl::archive::extract(
+											libftl::archive::file{
+												fcppt::reference_to_base<
+													std::istream
+												>(
+													fcppt::make_ref(
+														_stream
+													)
+												),
+												_entry.get()
+											}
+										),
+										[
+											&_path,
+											this
+										]{
+											return
+												libftl::error{
+													FCPPT_TEXT("Failed to read ")
+													+
+													fcppt::from_std_string(
+														_path.rep()
+													)
+													+
+													FCPPT_TEXT(" in ")
+													+
+													fcppt::filesystem::path_to_string(
+														this->file_path_
+													)
+												};
+										}
+									),
+									[](
+										fcppt::io::buffer &&_buffer
 									)
+									{
+										return
+											fcppt::unique_ptr_to_base<
+												std::istream
+											>(
+												// TODO: Use a streambuf that operates on io::buffer directly
+												fcppt::make_unique_ptr<
+													std::istringstream
+												>(
+													std::string{
+														_buffer.begin(),
+														_buffer.end()
+													}
+												)
+											);
+									}
 								);
 						}
 					);
