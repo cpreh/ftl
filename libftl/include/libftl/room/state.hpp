4704a4d35b37f852e327c413cba713b9e1dac5e4
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-09-02 10:21:33 +0200

    room::state constructor

--- libftl/include/libftl/room/state.hpp
+++ libftl/include/libftl/room/state.hpp
@@ -1,6 +1,7 @@
 #ifndef LIBFTL_ROOM_STATE_HPP_INCLUDED
 #define LIBFTL_ROOM_STATE_HPP_INCLUDED
 
+#include <libftl/detail/symbol.hpp>
 #include <libftl/room/state_fwd.hpp>
 #include <libftl/room/system.hpp>
 #include <fcppt/optional/object_impl.hpp>
@@ -13,6 +14,14 @@ namespace room
 
 struct state
 {
+	LIBFTL_DETAIL_SYMBOL
+	explicit
+	state(
+		fcppt::optional::object<
+			libftl::room::system
+		> &&
+	);
+
 	fcppt::optional::object<
 		libftl::room::system
 	> system_;
