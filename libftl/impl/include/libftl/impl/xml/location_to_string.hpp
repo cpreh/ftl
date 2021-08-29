59c1394b95ac4255c3422196716cc98546abaa67
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-08-29 10:37:34 +0200

    Improve location formatting

--- libftl/impl/include/libftl/impl/xml/location_to_string.hpp
+++ libftl/impl/include/libftl/impl/xml/location_to_string.hpp
@@ -1,21 +1,15 @@
 #ifndef LIBFTL_IMPL_XML_LOCATION_TO_STRING_HPP_INCLUDED
 #define LIBFTL_IMPL_XML_LOCATION_TO_STRING_HPP_INCLUDED
 
-#include <fcppt/output_to_fcppt_string.hpp>
+#include <libftl/detail/symbol.hpp>
 #include <fcppt/string.hpp>
-#include <fcppt/text.hpp>
-#include <fcppt/optional/object_impl.hpp>
-#include <fcppt/optional/output.hpp>
-#include <fcppt/parse/location_impl.hpp>
-#include <fcppt/parse/location_output.hpp>
+#include <fcppt/optional/object_fwd.hpp>
+#include <fcppt/parse/location_fwd.hpp>
 
 namespace libftl::impl::xml
 {
-[[nodiscard]] fcppt::string inline location_to_string(
-    fcppt::optional::object<fcppt::parse::location> const &_location)
-{
-  return fcppt::output_to_fcppt_string(_location) + FCPPT_TEXT(": ");
-}
+[[nodiscard]] LIBFTL_DETAIL_SYMBOL fcppt::string
+location_to_string(fcppt::optional::object<fcppt::parse::location> const &);
 }
 
 #endif
---
+++ libftl/impl/src/impl/xml/location_to_string.cpp
@@ -0,0 +1,24 @@
+#include <libftl/impl/xml/location_to_string.hpp>
+#include <fcppt/output_to_fcppt_string.hpp>
+#include <fcppt/string.hpp>
+#include <fcppt/text.hpp>
+#include <fcppt/optional/maybe.hpp>
+#include <fcppt/optional/object_impl.hpp>
+#include <fcppt/parse/location_impl.hpp>
+#include <fcppt/parse/location_output.hpp>
+
+fcppt::string libftl::impl::xml::location_to_string(
+    fcppt::optional::object<fcppt::parse::location> const &_location)
+{
+  return fcppt::optional::maybe(
+    _location,
+    []{
+      return fcppt::string{FCPPT_TEXT("Unknown location")};
+    },
+    [](fcppt::parse::location const &_loc)
+    {
+      return fcppt::output_to_fcppt_string(_loc);
+    }
+    )
+    + FCPPT_TEXT(": ");
+}
