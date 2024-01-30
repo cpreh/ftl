061369853c9491b41a91245282af13297b9cd9be
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-01-30 18:11:44 +0100

    Ignore another clang-tidy warning

--- libftl/include/libftl/ship/layout/tile_pos.hpp
+++ libftl/include/libftl/ship/layout/tile_pos.hpp
@@ -2,6 +2,6 @@
 #define LIBFTL_SHIP_LAYOUT_TILE_POS_HPP_INCLUDED
 
 #include <libftl/ship/layout/tile_pos_fwd.hpp> // IWYU pragma: export
-#include <fcppt/math/vector/static.hpp>
+#include <fcppt/math/vector/static.hpp> // IWYU pragma: keep
 
 #endif
